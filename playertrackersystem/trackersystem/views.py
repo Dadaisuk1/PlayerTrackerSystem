d3a04568616ea4e4c03a7d40179df0d4c7f35d6f
MutsarelaCheese <Darwindarryljean.largoza@gmail.com>
2024-11-22 01:45:46 +0800

    Game

--- playertrackersystem/trackersystem/views.py
+++ playertrackersystem/trackersystem/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Hero, Game
+from .forms import GameForm, HeroForm
 
 # Create your views here.
+def home_page(Httprequest):
+    games = Game.objects.all()
+    return render(Httprequest, 'home.html', {'games': games})
