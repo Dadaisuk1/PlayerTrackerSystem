18b8cfb99f9999d3f690aea2673959fa6ffae2dd
Seanicks <131010203+Seanicks@users.noreply.github.com>
2024-11-22 11:30:10 +0800

    inventory hehe

--- playertrackersystem/inventory/views.py
+++ playertrackersystem/inventory/views.py
@@ -5,7 +5,7 @@ from .forms import inventoryForms
 
 def item_list(request):
     items = inventory.objects.all()  # This should return all items
-    return render(request, 'item_app/item_list.html', {'items': items})
+    return render(request, 'inventory/item_list.html', {'items': items})
 
 
 def item_create(request):
@@ -16,11 +16,11 @@ def item_create(request):
             return redirect('item_list')
     else:
         form = inventoryForms()
-    return render(request, 'item_app/item_form.html', {'form': form})
+    return render(request, 'inventory/item_form.html', {'form': form})
 
 def item_detail(request, pk):
     item = get_object_or_404(inventory, pk=pk)
-    return render(request, 'item_app/item_detail.html', {'item': item})
+    return render(request, 'inventory/item_detail.html', {'item': item})
 
 def item_update(request, pk):
     item = get_object_or_404(inventory, pk=pk)
@@ -31,11 +31,11 @@ def item_update(request, pk):
             return redirect('item_detail', pk=item.pk)
     else:
         form = inventoryForms(instance=item)
-    return render(request, 'item_app/item_form.html', {'form': form})
+    return render(request, 'inventory/item_form.html', {'form': form})
 
 def item_delete(request, pk):
     item = get_object_or_404(inventory, pk=pk)
     if request.method == 'POST':
         item.delete()
         return redirect('item_list')
-    return render(request, 'item_app/item_confirm_delete.html', {'item': item})
+    return render(request, 'inventory/item_confirm_delete.html', {'item': item})
