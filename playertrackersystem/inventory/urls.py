c9af0a4994ea8c621115a5ffc129c09540ba1fb5
MutsarelaCheese <Darwindarryljean.largoza@gmail.com>
2024-11-22 11:43:21 +0800

    payche

--- playertrackersystem/inventory/urls.py
+++ playertrackersystem/inventory/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.item_list, name='item_list'),  # List view for items
+    # path('', views.item_list, name='item_list'),  # List view for items
     path('new/', views.item_create, name='item_create'),  # Create new item (corrected name)
     path('<int:pk>/', views.item_detail, name='item_detail'),  # Detail view for a specific item
     path('update/<int:pk>/', views.item_update, name='item_update'),  # Update an existing item
